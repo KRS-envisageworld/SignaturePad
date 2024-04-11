660db415038d3a177043d138ab007428ec1f4d16
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2024-04-12 02:06:30 +0530

    Update priview.component.ts

--- signaturepad_frontend/src/app/priview/priview.component.ts
+++ signaturepad_frontend/src/app/priview/priview.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-priview',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './priview.component.scss'
 })
 export class PriviewComponent {
+imgURL:string="";
+@Input() public image:string;
+  constructor(@Inject(MAT_DIALOG_DATA) public modalData:any,
+  public dialogRef: MatDialogRef<ImageEditorComponent>
+  ) { }
 
+  ngOnInit(): void {
+    debugger;
+    console.log(this.image);
+    this.imgURL = this.modalData.image;
+
+  }
+  download(){
+    var link = document.createElement("a");
+
+    document.body.appendChild(link); // for Firefox
+
+    link.setAttribute("href", this.imgURL);
+    link.setAttribute("download", "mrHankey.jpg");
+    link.click();
+ 
+  }
 }
