918582f43beb73ba3b45e2e5b242ab97e58250e6
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2024-04-12 02:07:33 +0530

    Update editorsettings.component.ts

--- signaturepad_frontend/src/app/editorsettings/editorsettings.component.ts
+++ signaturepad_frontend/src/app/editorsettings/editorsettings.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject,
+  Output,
+  Input,
+  OnInit,
+  EventEmitter,
+} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-editorsettings',
@@ -7,6 +15,36 @@ import { Component } from '@angular/core';
   templateUrl: './editorsettings.component.html',
   styleUrl: './editorsettings.component.scss'
 })
-export class EditorsettingsComponent {
+export class EditorsettingsComponent implements OnInit {
+  @Input() color: string = '';
+  @Input() strokeWidth: number = 0;
 
+  @Output() selectedColor = new EventEmitter<string>();
+  newColor: string = '';
+  formatLabel(value: number): string {
+    this.strokeWidth = value;
+
+    return `${value}`;
+  }
+ 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public modalData: any,
+    public dialogRef: MatDialogRef<DrawingsettingsComponent>
+  ) {
+    debugger
+    dialogRef.disableClose = true;
+    this.color = modalData.color;
+    this.strokeWidth = modalData.strokeWidth;
+
+  }
+
+  ngOnInit(): void {}
+
+  selectColor() {
+    this.newColor;
+  }
+  closeDialog() {
+    debugger;
+    this.dialogRef.close({ color: this.color, strokeWidth:this.strokeWidth });
+  }
 }
