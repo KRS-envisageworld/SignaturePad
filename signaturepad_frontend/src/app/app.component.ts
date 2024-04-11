6446150b0af8dd3cf3db07ee8b36666df9e9605c
KRS-envisageworld <83295488+KRS-envisageworld@users.noreply.github.com>
2024-04-12 01:59:00 +0530

    Update app.component.ts

--- signaturepad_frontend/src/app/app.component.ts
+++ signaturepad_frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test';
+  title = 'signaturepad_frontend';
 }
