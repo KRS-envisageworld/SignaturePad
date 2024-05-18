1e7345baae9a4bd3e3057bdfbfd79d9babcce427
iamkrs <envisageworld@gmail.com>
2024-05-19 04:56:31 +0530

    Added sidenav

--- signaturepad_frontend/src/app/app.component.ts
+++ signaturepad_frontend/src/app/app.component.ts
@@ -2,10 +2,31 @@ import { Component,inject  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, VERSION } from '@angular/router';
 
+
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { SignaturepadComponent } from './signaturepad/signaturepad.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    SignaturepadComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -13,4 +34,12 @@ export class AppComponent {
   title = 'signaturepad_frontend';
   version = VERSION.full
   private router: Router = inject(Router)
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
